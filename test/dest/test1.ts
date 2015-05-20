e4f7a247a668cabdac28c17465e3960b05490f23
Hitoshi Honma <honma_hitoshi@sirok.co.jp>
2015-05-20 21:45:12 +0900

    Rebuild

--- test/dest/test1.ts
+++ test/dest/test1.ts
@@ -1,3 +1,3 @@
-/// <reference path="../../ts/src/test1.ts"/>
-/// <reference path="../../ts/src/test2.ts"/>
-/// <reference path="../../ts/src/a/test3.ts"/>
+/// <reference path="../src/test1.ts"/>
+/// <reference path="../src/test2.ts"/>
+/// <reference path="../src/a/test3.ts"/>
